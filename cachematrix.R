1fa4cac37f6c46d83661b395cb9b1a2312612f15
tesszty <eszter.takacs.hu@gmail.com>
2015-10-25 17:53:37 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -19,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
 
     inv <- x$getinverse()
     if(!is.null(inv)) {
